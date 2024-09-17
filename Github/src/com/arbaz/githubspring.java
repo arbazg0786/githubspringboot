38f144fa43f5341668e00c7d916be1fae573a9d1
Arbaz Ansari <220101120021@cutm.ac.in>
2024-09-17 14:48:40 +0530

    Update githubspring.java

--- Github/src/com/arbaz/githubspring.java
+++ Github/src/com/arbaz/githubspring.java
@@ -3,7 +3,7 @@ package com.arbaz;
 public class githubspring {
 
 	public static void main(String[] args) {
-		System.out.println("hello duniya lelelel");
+		System.out.println("hello duniya ");
 		// TODO Auto-generated method stub
 
 	}
